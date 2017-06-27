611541e992465b5ce6f7f82598a64c5ffa6ad3f6
Leandro Voltolino <xupisco@gmail.com>
2017-06-27 13:54:48 -0300

    removed first scene load

--- main.lua
+++ main.lua
@@ -14,4 +14,4 @@ composer.recycleOnSceneChange = true
 composer.effectList.fade.from.transition = easing.outQuad
 composer.effectList.fade.to.transition = easing.outQuad
 
-composer.gotoScene('scenes.menu')
+--composer.gotoScene('scenes.menu')
